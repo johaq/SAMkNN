0e676ba3659f22a63d98fd256ec7a6ac0ea9046e
jkummert <jkummert@techfak.uni-bielefeld.de>
2022-02-18 14:03:10 +0100

    added data loader

--- SAMKNN/SAMKNNTest.py
+++ SAMKNN/SAMKNNTest.py
@@ -6,6 +6,8 @@ from sklearn.metrics import accuracy_score
 from ClassifierVisualizer import ClassifierVisualizer
 from ClassifierListener import DummyClassifierListener
 import logging
+import sys
+from data_loader import DataLoader
 
 def run(X, y, hyperParams, visualize=False):
     """
@@ -28,7 +30,7 @@ def run(X, y, hyperParams, visualize=False):
 if __name__ == '__main__':
     logging.basicConfig(format='%(message)s', level=logging.DEBUG)
     hyperParams ={'maxSize': 1000, 'nNeighbours': 3, 'knnWeights': 'distance', 'recalculateSTMError': False,
-                  'useLTM': True, 'metric': 'LMNN', 'metric_step': 500}
+                  'useLTM': True, 'metric': 'LMNN', 'metric_step': 100}
     #hyperParams = {'windowSize': 5000, 'nNeighbours': 5, 'knnWeights': 'distance', 'STMSizeAdaption': None,
     #               'useLTM': False}
 
@@ -39,10 +41,31 @@ if __name__ == '__main__':
     #X = np.loadtxt('../datasets/movingSquares.data')
     #y = np.loadtxt('../datasets/movingSquares.labels', dtype=np.uint8)
 
-    X = np.loadtxt('../../data/driftDatasets/artificial/chess/transientChessboard.data')
-    y = np.loadtxt('../../data/driftDatasets/artificial/chess/transientChessboard.labels', dtype=np.uint8)
+    data = sys.argv[1]
+    loader = DataLoader("../../")
+    print("Data Set: " + data)
+    if data == "electricity":
+        X, y = loader.load_electricity()
+    elif data == "outdoor":
+        X, y = loader.load_outdoor()
+    elif data == "poker":
+        X, y = loader.load_poker()
+    elif data == "rialto":
+        X, y = loader.load_rialto()
+    elif data == "chess":
+        X, y = loader.load_chess()
+    elif data == "hyperplane":
+        X, y = loader.load_hyperplane()
+    elif data == "mixed_drift":
+        X, y = loader.load_mixed_drift()
+    elif data == "moving_squares":
+        X, y = loader.load_moving_squares()
+    elif data == "interchanging_rbf":
+        X, y = loader.load_interchanging_rbf()
+    elif data == "moving_rbf":
+        X, y = loader.load_moving_rbf()
 
     logging.info('%d samples' % X.shape[0])
     logging.info('%d dimensions' % X.shape[1])
-    run(X, y, hyperParams, visualize=True)
+    run(X, y, hyperParams, visualize=False)
 
---
+++ SAMKNN/data_loader.py
@@ -0,0 +1,74 @@
+import numpy as np
+from scipy.io import arff
+
+
+class DataLoader:
+    def __init__(self, data_folder_path):
+        self.data_folder_path = data_folder_path
+
+    def load_data_file(self, data_path, label_path):
+        X = np.loadtxt(self.data_folder_path + data_path, dtype=float)
+        Y = np.loadtxt(self.data_folder_path + label_path, dtype=float)
+        return X, Y
+
+    def load_arff_file(self):
+        data_file = "./data/elecNormNew.arff"
+        data, meta = arff.loadarff(data_file)
+
+        data_list_x = []
+        data_list_y = []
+
+        for d in data:
+            data_point = []
+            for i in range(8):
+                data_point.append((float)(d[i]))
+            data_list_x.append(data_point)
+            if d[8] == b'UP':
+                data_list_y.append(1)
+            else:
+                data_list_y.append(0)
+
+        X = np.array(data_list_x)
+        Y = np.array(data_list_y)
+
+        X = X.astype(np.float16)
+
+    def load_chess(self):
+        return self.load_data_file("./data/driftDatasets/artificial/chess/transientChessboard.data",
+                              "./data/driftDatasets/artificial/chess/transientChessboard.labels")
+
+    def load_hyperplane(self):
+        return self.load_data_file("./data/driftDatasets/artificial/hyperplane/rotatingHyperplane.data",
+                              "./data/driftDatasets/artificial/hyperplane/rotatingHyperplane.labels")
+
+    def load_mixed_drift(self):
+        return self.load_data_file("./data/driftDatasets/artificial/mixedDrift/mixedDrift.data",
+                              "./data/driftDatasets/artificial/mixedDrift/mixedDrift.labels")
+
+    def load_moving_squares(self):
+        return self.load_data_file("./data/driftDatasets/artificial/movingSquares/movingSquares.data",
+                              "./data/driftDatasets/artificial/movingSquares/movingSquares.labels")
+
+    def load_interchanging_rbf(self):
+        return self.load_data_file("./data/driftDatasets/artificial/rbf/interchangingRBF.data",
+                              "./data/driftDatasets/artificial/rbf/interchangingRBF.labels")
+
+    def load_moving_rbf(self):
+        return self.load_data_file("./data/driftDatasets/artificial/rbf/movingRBF.data",
+                              "./data/driftDatasets/artificial/rbf/movingRBF.labels")
+
+    def load_electricity(self):
+        return self.load_data_file("./data/driftDatasets/realWorld/Elec2/elec2_data.dat",
+                              "./data/driftDatasets/realWorld/Elec2/elec2_label.dat")
+
+    def load_outdoor(self):
+        return self.load_data_file("./data/driftDatasets/realWorld/outdoor/outdoorStream.data",
+                              "./data/driftDatasets/realWorld/outdoor/outdoorStream.labels")
+
+    def load_poker(self):
+        return self.load_data_file("./data/driftDatasets/realWorld/poker/poker.data",
+                              "./data/driftDatasets/realWorld/poker/poker.labels")
+
+    def load_rialto(self):
+        return self.load_data_file("./data/driftDatasets/realWorld/rialto/rialto.data",
+                              "./data/driftDatasets/realWorld/rialto/rialto.labels")
